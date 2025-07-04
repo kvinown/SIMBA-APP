ff9a6277a5cb8f0ea87328abfe296ef7481b6e58
Kevin Owen <113487713+kvinown@users.noreply.github.com>
2025-07-05 01:26:57 +0700

    Update sidebar layout and navigation styles
    Added Bootstrap flex utilities to the sidebar for improved layout. Updated navigation links to use button styles for better appearance and consistency.

--- resources/views/app_layouts/sidebar.blade.php
+++ resources/views/app_layouts/sidebar.blade.php
@@ -1,13 +1,10 @@
-<div class="bg-dark text-white p-3 sidebar" id="sidebar">
+<div class="bg-dark text-white p-3 sidebar d-flex flex-column" id="sidebar">
     <h4>Simba-App</h4>
     <h5>@if(Auth::check()) {{ Auth::user()->name }} @else Guest @endif</h5>
-    <ul class="nav flex-column">
-        <li class="nav-item"><a href="{{ route('schedule.index') }}" class="nav-link text-white">Schedule</a></li>
-{{--        <li class="nav-item"><a href="{{ route('dash') }}" class="nav-link text-white">Dashboard</a></li>--}}
-{{--        <li class="nav-item"><a href="{{ route('example') }}" class="nav-link text-white">Example</a></li>--}}
-{{--        <li class="nav-item"><a href="{{ route('lecturer.index') }}" class="nav-link text-white">Lecturer</a></li>--}}
-{{--        <li class="nav-item"><a href="{{ route('period.index') }}" class="nav-link text-white">Academic Period</a></li>--}}
-{{--        <li class="nav-item"><a href="{{ route('enrollment.index') }}" class="nav-link text-white">Enrollment</a></li>--}}
+    <ul class="nav flex-column flex-grow-1" >
+        <li class="nav-item"><a href="{{ route('schedule.index') }}" class="btn btn-light w-100 my-1">Schedule</a></li>
+
+
         <!-- Logout Button -->
         <li class="nav-item mt-auto">
             <form method="POST" action="{{ route('logout') }}">
